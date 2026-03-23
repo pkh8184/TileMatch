b4a04b24e244d72fec6b28ccdae20f95c17c81c8
skyGom <dprha4789@gmail.com>
2026-03-23 22:04:11 +0900

    [Tile-Client-001]

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/Core/SlotManager.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/Core/SlotManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using TrumpTile.GameMain.Data;
+using TrumpTile.GameMain.UI;
 
 namespace TrumpTile.GameMain.Core
 {
