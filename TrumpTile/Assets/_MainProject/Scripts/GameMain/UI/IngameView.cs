e66ea6ae8cc7bc281a4d83c4e62231b9af7ec378
rhkrdnjswns <dnjswns-11@naver.com>
2026-05-05 18:18:52 +0900

    [Tile-Client-020] GameScene UI 애니메이션 추가, 스크립트 수정

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/IngameView.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/IngameView.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Collections;
 using TrumpTile.GameMain.Core;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 namespace TrumpTile.GameMain.UI
 {
     public class IngameView : ViewBase
     {
+        [Header("스테이지 시작 시 표시될 레벨네임 오브젝트들")]
+        [SerializeField] private CanvasGroup mLevelNameCanvasGroup;
+        [SerializeField] private RectTransform mLevelNameRect;
+        
         public override void Initialize()
         {
             base.Initialize();
-
+            mLevelNameCanvasGroup.alpha = 0;
             //임시
             EventManager.Inst.AddEvent("IngameLoadingComplete", PlayFadeInAfterLoadLevel);
         }
@@ -26,6 +32,27 @@ namespace TrumpTile.GameMain.UI
             yield return StartCoroutine(Co_FadeInAnim());
 
             onComplete?.Invoke();
+
+            mLevelNameCanvasGroup.transform.GetChild(0).GetComponent<TMP_Text>().text = $"LEVEL {GameManager.Instance.CurrentLevel}";
+            StartCoroutine(Co_PlayLevelNameAnim());
+            
+        }
+        private IEnumerator Co_PlayLevelNameAnim()
+        {
+            Sequence sq = DOTween.Sequence();
+
+            sq.Append(mLevelNameCanvasGroup.DOFade(1, 0.5f));
+            RectTransform rt = mLevelNameCanvasGroup.GetComponent<RectTransform>();
+            sq.Join(rt.DOPunchScale(Vector3.one * 0.1f, 0.4f, 5, 0.5f));
+            sq.AppendInterval(0.5f);
+
+            float targetX = -(Screen.width / 2f + rt.rect.width);
+            sq.Append(rt.DOAnchorPosX(targetX, 0.5f).SetEase(Ease.InQuad));
+
+            sq.AppendInterval(0.3f);
+            sq.Append(mLevelNameRect.DOAnchorPosX(0, 0.15f).SetEase(Ease.OutQuad));
+
+            yield return sq.WaitForCompletion();
         }
     }
     
