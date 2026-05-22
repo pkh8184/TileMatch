65bc1a8df1c713808917f1e35efa745cf76ac0d3
rhkrdnjswns <dnjswns-11@naver.com>
2026-05-22 11:48:06 +0900

    [클라] 컨텐츠 UI 컨트롤러 컴포넌트 추가

---
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/ContentUIController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrumpTile.GameMain.UI
+{
+    [System.Serializable]
+    public class ContentUIController
+    {
+        public virtual void PlayShowButtonAnim(Button button){}
+    }    
+}
+
