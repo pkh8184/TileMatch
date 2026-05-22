c206dfa7fce67a16ed031cd4cf0dcb50cc5321fb
rhkrdnjswns <dnjswns-11@naver.com>
2026-05-22 12:43:08 +0900

    [클라] RoulettePopup 연출 추가

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RoulettePopup.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RoulettePopup.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace TrumpTile.GameMain.UI
 {
     public class RoulettePopup : PopupBase
     {
-        
+        [Header("애니메이션을 위한 참조")]
+        [SerializeField] private RectTransform mRouletteRect;
+        protected override void PlayShowAnim()
+        {
+            base.PlayShowAnim();
+
+            Sequence seq = DOTween.Sequence();
+            mRouletteRect.localRotation = Quaternion.Euler(0,0,-207);
+
+            seq.Append(mRouletteRect.DORotate(new Vector3(0,0,-242), 1f).SetEase(Ease.OutQuart));
+        }
     }    
 }
 
