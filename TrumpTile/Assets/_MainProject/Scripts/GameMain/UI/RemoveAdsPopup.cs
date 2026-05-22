697fd2ae6e8157cd40e95bda24e587f0dfc9448e
rhkrdnjswns <dnjswns-11@naver.com>
2026-05-22 12:33:44 +0900

    [클라] RemoveAdsPopup 연출 추가

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RemoveAdsPopup.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RemoveAdsPopup.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace TrumpTile.GameMain.UI
 {
     public class RemoveAdsPopup : PopupBase
     {
+        [Header("오브젝트 애니메이션을 위한 참조")]
+        [SerializeField] private RectTransform mIconRect;
         [Header("컨텐츠 UI 전용 컴포넌트")]
         [SerializeField] private PermanentContentUIController mContentController;
-
+        private Sequence mSeq;
         public override void Initialize()
         {
             base.Initialize();
 
             mContentController.PlayShowButtonAnim(mShowButton);
+        }
+        protected override void PlayShowAnim()
+        {
+            base.PlayShowAnim();
+
+            if(mSeq != null && mSeq.active)
+            {
+                mSeq.Kill();
+            }
+            mIconRect.anchoredPosition = Vector2.zero;
+            mIconRect.localScale = Vector2.one;
+
+            mSeq = DOTween.Sequence();
+            mSeq.Append(mIconRect.DOAnchorPosY(30, 1.5f).SetEase(Ease.InOutSine));
+            mSeq.Append(mIconRect.DOAnchorPosY(0, 1.5f).SetEase(Ease.InOutSine));
+            mSeq.Insert(0, mIconRect.DOScale(Vector2.one * 1.1f, 1f).SetEase(Ease.InOutSine));
+            mSeq.Insert(1f, mIconRect.DOScale(Vector2.one, 1f).SetEase(Ease.InOutSine));
+
+            mSeq.SetLoops(-1);
         }
     }    
 }
