2cd72285d5f5383ad5f98d9346603ac5b5fa03f5
rhkrdnjswns <dnjswns-11@naver.com>
2026-06-06 21:58:56 +0900

    [클라] 컨텐츠 베이스 구조 작성, 출석체크 컨텐츠 스크립트 작성

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/Core/TemporaryContent.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/Core/TemporaryContent.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 namespace TrumpTile.GameMain.Core
 {
-    [CreateAssetMenu(fileName = "Content", menuName = "TrumpTile/TemporaryContent")]
-    //[Serializable]
+    [Serializable]
     public class TemporaryContent : ContentBase
     {
+        [Header("컨텐츠 제한시간")]
         [SerializeField] protected float mLimitTime;
-        [SerializeField] protected float mCoolTime;
+        [Header("컨텐츠 종료 후 재시작까지 쿨타임")]
+        [SerializeField] private float mCoolTime;
+
+        //컨텐츠 활성화 플래그
+        protected bool mbIsActive;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            //플레이어 데이터에서 컨텐츠에 해당하는 시간 정보 읽어와서 활성화 / 비활성화 처리
+        }
     }   
 }
