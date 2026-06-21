13df00a0ea9b44a513d9d5aefafa668b09a47f32
rhkrdnjswns <dnjswns-11@naver.com>
2026-06-22 06:14:10 +0900

    [클라] 스테이지 실패, 스테이지 나가기 팝업에서 일일 퍼즐, 챔피언스 리그로 표기되지 않던 부분 수정

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/IngameFailedPopup.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/IngameFailedPopup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using TrumpTile.GameMain.Core;
+using TrumpTile.GameMain.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,7 +29,17 @@ namespace TrumpTile.GameMain.UI
         }
         public override void Show()
         {
-            mLevelText.text = "레벨 " + GameManager.Instance.CurrentLevel.ToString();
+            bool bIsDailyMode = DailyPuzzleManager.Inst != null && DailyPuzzleManager.Inst.IsActive;
+			if (bIsDailyMode)
+			{
+				mLevelText.text = "일일 퍼즐";
+			}
+			else
+			{
+                string header = GameManager.Instance.IsChampionsMode ? "챌린지 " : "레벨 ";
+                string number = GameManager.Instance.IsChampionsMode ? PlayerDataManager.Inst.ChampionsLevel.ToString() : GameManager.Instance.CurrentLevel.ToString();
+                mLevelText.text = header + number;
+            }
             AudioEvent.Play(EAudioKey.SFX_StageLosed_02);
             base.Show();
         }
