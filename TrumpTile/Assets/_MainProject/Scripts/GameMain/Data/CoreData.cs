4486b25e9bbbea50d145b546750a865483b62cd8
rhkrdnjswns <dnjswns-11@naver.com>
2026-06-22 06:10:13 +0900

    [Tile-Client-033] 챔피언스 리그용 레벨 데이터 생성 및 어드레서블 설정

---
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/Data/CoreData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrumpTile.GameMain.Core
+{
+    public static class CoreData
+    {
+        public const int MAX_STAGE = 2;
+        public const int CHAMPIONS_INTERVAL = 11;
+    }    
+}
+
