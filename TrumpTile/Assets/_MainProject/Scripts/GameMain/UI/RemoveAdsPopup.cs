7cb6f016ba157006628af51e0b5c4533c98d4055
rhkrdnjswns <dnjswns-11@naver.com>
2026-06-21 22:01:31 +0900

    [클라] 광고 제거 패키지 구매한 경우 광고 제거 패키지 팝업 버튼 메인에 표시 안되도록 수정

--- TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RemoveAdsPopup.cs
+++ TrumpTile/Assets/_MainProject/Scripts/GameMain/UI/RemoveAdsPopup.cs
@@ -34,6 +34,11 @@ namespace TrumpTile.GameMain.UI
         {
             base.Initialize();
 
+            if(PlayerDataManager.Inst.IsAdsRemoved)
+            {
+                mShowButton.gameObject.SetActive(false);
+                return;
+            }
             mContentController.PlayShowButtonAnim(mShowButton);
 
             if(mPurchaseButton != null)
