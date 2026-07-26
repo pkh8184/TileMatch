def6f8a2bcb833b042567e5667859d67e1ca5917
rhkrdnjswns <dnjswns-11@naver.com>
2026-07-27 06:55:09 +0900

    [클라] FirebaseFunctions 배포를 위한 수정

--- TileMatch_FirebaseFunctions/functions/.eslintrc.js
+++ TileMatch_FirebaseFunctions/functions/.eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    "ecmaVersion": 2021,
   },
   extends: [
     "eslint:recommended",
-    "google",
   ],
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
